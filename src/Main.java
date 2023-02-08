916d6e136c140cb8df9513824fde2fc174111f44
yanastyas <yanastyas28@mail.ru>
2023-02-09 00:35:42 +1000

    поправки

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,15 @@
 import java.util.Collections;
 import java.util.List;
 import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         List<Person> people = new ArrayList<>();
-        people.add((new Person("Алёна","Панченко",62)));
-        people.add((new Person("Камила","Адуева",39)));
-        people.add((new Person("Наталья","Бабкина",45)));
-        people.add((new Person("Галина","Виноградская-Козулина",57)));
+        people.add((new Person("Алёна", "Панченко", 62)));
+        people.add((new Person("Камила", "Адуева", 39)));
+        people.add((new Person("Наталья", "Бабкина", 45)));
+        people.add((new Person("Галина", "Виноградская-Козулина", 57)));
+        people.add((new Person("Илья", "Конник", 33)));
         Collections.sort(people, new PersonSurnameComparator(1));
         System.out.println(people);
         Collections.sort(people, new PersonSurnameComparator(2));
--- src/PersonSurnameComparator.java
+++ src/PersonSurnameComparator.java
@@ -1,18 +1,18 @@
 import java.util.Comparator;
 
 public class PersonSurnameComparator implements Comparator<Person> {
-    public int number;
+    protected int maxWord;
 
-    public PersonSurnameComparator(int number) {
-        this.number = number;
+    public PersonSurnameComparator(int maxWord) {
+        this.maxWord = maxWord;
     }
 
     @Override
     public int compare(Person o1, Person o2) {
         String[] surname1 = o1.detSurname().split("\\P{IsAlphabetic}+");
         String[] surname2 = o2.detSurname().split("\\P{IsAlphabetic}+");
-        int surnameLength1 = Math.min(surname1.length, number);
-        int surnameLength2 = Math.min(surname2.length, number);
+        int surnameLength1 = Math.min(surname1.length, maxWord);
+        int surnameLength2 = Math.min(surname2.length, maxWord);
         int surnameCompare = Integer.compare(surnameLength2, surnameLength1);
         if (surnameCompare == 0) {
             return Integer.compare(o2.getAge(), o1.getAge());
