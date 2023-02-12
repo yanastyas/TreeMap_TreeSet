c360f3322078bde9971bc0defc387519aa9dc783
yanastyas <yanastyas28@mail.ru>
2023-02-12 20:58:12 +1000

    задача1

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,6 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.ArrayList;
-
 public class Main {
     public static void main(String[] args) {
         List<Person> people = new ArrayList<>();
